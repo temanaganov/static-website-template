53593c37ff6ee5713b1e23374676952181a966cd
Artem Naganov <artemnaganov10@gmail.com>
2021-05-24 11:19:49 +0300

    Add plop generator for components

--- plopfile.js
+++ plopfile.js
@@ -25,29 +25,24 @@ module.exports = function (plop) {
             },
         ],
     });
-    plop.setGenerator('Paвыафыаавыфаge', {
-        description: 'ывафыафвы it',
+    plop.setGenerator('Component', {
+        description: 'Create a pug template of component and styles for it',
         prompts: [
             {
                 type: 'input',
-                name: 'name',
-                message: 'Page name (kebab-case)',
-            },
-            {
-                type: 'input',
                 name: 'title',
-                message: 'Page title',
+                message: 'Component title (kebab-case)',
             },
         ],
         actions: [
             {
                 type: 'add',
-                path: 'src/pug/pages/{{name}}/index.pug',
-                templateFile: '.plop/page-template.hbs',
+                path: 'src/pug/components/{{title}}.pug',
+                templateFile: '.plop/component-template.hbs',
             },
             {
                 type: 'add',
-                path: 'src/styles/{{name}}.scss',
+                path: 'src/styles/components/_{{title}}.scss',
             },
         ],
     });
